541a60aae3a8b58e83b1826d1f8fddbdf7a0ad0c
MassimoEsperto <43523530+MassimoEsperto@users.noreply.github.com>
2023-11-21 09:28:49 +0100

    src

---
+++ src/environments/enums.ts
@@ -0,0 +1,35 @@
+export enum FasiCompetizione {
+    GIRONI = 1,
+    SPAREGGI = 2,
+    OTTAVI = 3,
+    QUARTI = 4,
+    SEMI_FINALE = 5,
+    FINALE = 6
+}
+
+export enum PeriodoGiornata {
+    PRE_PARTITA = 1,
+    PARTITA_LIVE = 2,
+    POST_PARTITA = 3
+}
+
+
+export enum StatiSquadra {
+    REGISTRATA = 1,
+    PARTECIPANTE = 2,
+    PAGANTE = 3,
+    ELIMINATO = 4,
+    VINCITORE = 5
+}
+
+export enum RuoliUtente {
+    GHOST = 1,
+    PLAYER = 2,
+    ADMIN = 3
+}
+
+export enum CondizioneGirone {
+    QAULIFICATO = 1,
+    SPAREGGI = 2,
+    ELIMINATA = 3
+}
